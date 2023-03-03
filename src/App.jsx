91e2244be533b9cbd2e7b533024b00e2a2426dff
YURII SILIN <try2scumme@gmail.com>
2023-03-03 12:57:50 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Patriot from "./components/Patriot";
 
 import Navbar from "./components/navbar";
@@ -9,16 +8,6 @@ import TimeLine from "./components/TimeLine";
 import BelarusMap from "./components/BelarusMap";
 
 function App() {
-  // useEffect(() => {
-  //   // const element = document.getElementsByClassName("avtor");
-  //   setTimeout(() => {
-  //     const element = document.getElementByClassName("avtor");
-  //     element.className += " newClass";
-  //   }, 1000);
-  //   console.log(element);
-  //   // element.parentNode.removeChild(div);
-  //   // element.remove(); // Removes the div with the 'div-02' id
-  // }, [element]);
   return (
     <div className="App">
       {/* <Navbar /> */}
