2acb4600dbe3514bb9e8d0e93085de0bb1c4aced
YURII SILIN <try2scumme@gmail.com>
2023-03-10 07:10:08 +0300

    1

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,15 +3,29 @@ import React from "react";
 function index() {
   return (
     <div className=" flex justify-between p-3 w-8/12 m-auto">
-      <div className="">Патриот</div>
+      <div className="">
+        <p>Патриот</p>
+        <p>Новости</p>
+        <p>Незаживающие раны памяти</p>
+        <p>Поэтическая карта</p>
+        <p>Маршруты выходного дня</p>
+        <p>Фотомузей «Беларуская хатка»</p>
+        <p>Игры</p>
+      </div>
+      <div className="flex flex-col">
+        Обратная связь
+        <input />
+        <button>Отправить</button>
+      </div>
 
       <a
         href="https://github.com/YuriAndreevich"
         target="_blank"
         rel="noreferrer"
+        className="flex"
       >
         <img
-          className="h-12"
+          className="h-12 content-center "
           src="https://preview.colorlib.com/theme/stellar/images/clients/icon-github.svg"
           alt="GitHub"
         />
